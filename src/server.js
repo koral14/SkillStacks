73f3e348d8717ceb26c1afd97f5db17ef51dc158
koral14 <105457134+koral14@users.noreply.github.com>
2023-12-16 14:40:14 -0800

    updated the server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { PORT = 8000 } = process.env;
+const { PORT = 8000, REACT_APP_REACT_URL } = process.env;
 const app = require("./app");
 const connectDB = require('./db/connect');
 
@@ -9,6 +9,7 @@ const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
         app.listen(PORT, listener);
+        console.log(`React app URL: ${REACT_APP_REACT_URL}`);
     }  catch (error) {
         console.log(error);
     }
