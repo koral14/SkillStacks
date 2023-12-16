37b14f2375a057817f3234bae383092d5a609f70
koral14 <105457134+koral14@users.noreply.github.com>
2023-12-16 14:53:47 -0800

    server

--- src/server.js
+++ src/server.js
@@ -3,13 +3,15 @@ const { REACT_APP_REACT_URL } = process.env;
 const app = require("./app");
 const connectDB = require('./db/connect');
 
-const listener = () => console.log(`Listening on Port ${REACT_APP_REACT_URL}!`);
+// const listener = () => console.log(`Listening on Port ${REACT_APP_REACT_URL}!`);
 
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
-        app.listen(REACT_APP_REACT_URL, listener);
-        console.log(`React app URL: ${REACT_APP_REACT_URL}`);
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Listening on Port ${process.env.PORT || 8000}!`);
+            console.log(`React app URL: ${REACT_APP_REACT_URL}`);
+        });
     }  catch (error) {
         console.log(error);
     }
