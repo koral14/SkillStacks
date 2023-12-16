61c346a965aa4e5f161834690310ee32e56cad66
koral14 <105457134+koral14@users.noreply.github.com>
2023-12-16 15:04:18 -0800

    server

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
-const { REACT_APP_REACT_URL } = process.env;
+const { FLASHCARDS_API_BASE_URL } = process.env;
 const app = require("./app");
 const connectDB = require('./db/connect');
 
-const listener = () => console.log(`Listening on ${REACT_APP_REACT_URL}!`);
+const listener = () => console.log(`Listening on ${FLASHCARDS_API_BASE_URL}!`);
 
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
-        app.listen(REACT_APP_REACT_URL, listener);
-        console.log(`React app URL: ${REACT_APP_REACT_URL}`);
+        app.listen(FLASHCARDS_API_BASE_URL, listener);
+        console.log(`React app URL: ${FLASHCARDS_API_BASE_URL}`);
     }  catch (error) {
         console.log(error);
     }
