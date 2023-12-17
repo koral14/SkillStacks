9ca1afbdb96c03ab98dfa13da4edab9278da5688
koral14 <105457134+koral14@users.noreply.github.com>
2023-12-16 22:33:30 -0800

    server changes

--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
-const { FLASHCARDS_API_BASE_URL } = process.env;
+const { PORT = 8000 } = process.env;
 const app = require("./app");
 const connectDB = require('./db/connect');
 
-const listener = () => console.log(`Listening on ${FLASHCARDS_API_BASE_URL}!`);
+const listener = () => console.log(`Listening on Port ${PORT}!`);
 
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
-        app.listen(FLASHCARDS_API_BASE_URL, listener);
-        console.log(`React app URL: ${FLASHCARDS_API_BASE_URL}`);
+        app.listen(PORT, listener);
     }  catch (error) {
         console.log(error);
     }
